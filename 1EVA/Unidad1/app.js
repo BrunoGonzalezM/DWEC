8b1ce2ce25095fd194538872fdd831e2bae4822b
BrunoGonzalezM <alumno.1355082@ies-azarquiel.es>
2023-09-21 17:37:30 +0200

    Mensaje de guardar archivos xd

---
+++ 1EVA/Unidad1/app.js
@@ -0,0 +1,32 @@
+console.log("Hola mundo!");                     //Mensaje general
+console.info("Esto es un mensaje informativo"); //Información
+console.warn("Cuidadito");                      //Aviso
+console.error("Mensaje de error");              //Error
+
+//alert("Mensaje");
+//confirm("Mensaje de confirmación");
+//prompt("Mensaje de prompt"); 
+
+var num=5;      //Variable tradicional. (Visible en todo el script / GLOBAL)
+let numero=8;   //Variable accesible solo a su scope (Visible dentro de su ambito)
+const pi=3.14;  //Variable constante, no cambia. (Accesible a su scope)
+
+/*
+Tipos de datos en javascript:
+
+    Number: Número.
+    String: Cadena de caracteres.
+    Boolean: Verdadero o Falso.
+    NaN: Not a Number.
+    Undefined: No definido.
+    Null: Nulo.
+
+Operaciones aritmeticas:
+
+    + - * / % (Suma | Resta | Multiplicación | División | Modulo)
+    < > (Menor a | Mayor a)
+    <= >= (Menor o igual a | Mayor o igual a)
+    == != (Igual a | No igual a)
+    === (Son iguales e incluso de tipo de dato)
+
+*/

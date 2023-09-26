89d92b442b61be8a9739af0ecfa7763f3373456d
BrunoGonzalezM <alumno.1355082@ies-azarquiel.es>
2023-09-26 19:44:05 +0200

    Mensaje de guardado

--- 1EVA/Ejercicio_Sintaxis_basica_de_JS/app4.js
+++ 1EVA/Ejercicio_Sintaxis_basica_de_JS/app4.js
@@ -1,10 +1,10 @@
-let correoElectronico=prompt("Ingrese un correo electronico :");
-if(correoElectronico==''){
-    console.log("No se ha introducido ningun correo");
-    resultado="No se ha introducido ningun correo";
+let correoElectronico=prompt("Ingrese un correo electronico :"); // Pide al usuario que ingrese un correo electrónico y lo almacena en la variable "correoElectronico".
+if(correoElectronico==''){// Comprueba si el correo electrónico ingresado está vacío.
+    console.log("No se ha introducido ningun correo"); // Imprime un mensaje en la consola si el correo electrónico está vacío.
+    resultado="No se ha introducido ningun correo"; // Asigna un mensaje a la variable "resultado".
 }else{
-    console.log(correoElectronico.split('@')[0].trim());
-    resultado=correoElectronico.split('@')[0].trim();
+    console.log(correoElectronico.split('@')[0].trim());// Si el correo no está vacío, imprime la parte antes del "@" después de quitar espacios en blanco.
+    resultado=correoElectronico.split('@')[0].trim();// Asigna la parte antes del "@" después de quitar espacios en blanco a la variable "resultado".
 }
-resultadoElement = document.getElementById("resultado");
-resultadoElement.innerText = resultado;
+resultadoElement = document.getElementById("resultado");// Obtiene el elemento HTML con el id "resultado".
+resultadoElement.innerText = resultado; // Asigna el valor de la variable "resultado" como el texto del elemento HTML con id "resultado".
--- 1EVA/Ejercicio_Sintaxis_basica_de_JS/app5.js
+++ 1EVA/Ejercicio_Sintaxis_basica_de_JS/app5.js
@@ -1,23 +1,23 @@
-let resultado = " ";
-let num = prompt("Ingrese un numero: ").trim();
-if (num === null || num === '') {
-    console.warn("No ingreso un numero");
-    resultado = "No ingreso un numero";
-} else if (isNaN(num) || parseInt(num) < 0) {
-    console.warn("No es un numero valido.");
-    resultado = "No es un numero valido.";
+let resultado = " ";// Crea una variable llamada 'resultado' y la inicializa con un espacio en blanco.
+let num = prompt("Ingrese un numero: ").trim();// Solicita al usuario que ingrese un número y lo almacena en 'num', eliminando espacios al inicio y al final.
+if (num === null || num === '') { // Comprueba si 'num' es nulo o está vacío.
+    console.warn("No ingreso un numero");  // Muestra un mensaje de advertencia si el usuario no ingresó un número.
+    resultado = "No ingreso un numero";// Asigna un mensaje a 'resultado' indicando que el usuario no ingresó un número.
+} else if (isNaN(num) || parseInt(num) < 0) {// Comprueba si 'num' no es un número o es un número negativo.
+    console.warn("No es un numero valido.");// Muestra un mensaje de advertencia si 'num' no es un número válido.
+    resultado = "No es un numero valido.";// Asigna un mensaje a 'resultado' indicando que el número ingresado no es válido.
 } else {
-    num = num.trim();
-    const ul = document.createElement('ul');
-    for (let i = 0; i <= 10; i++) {
-        const multiplicacion = num * i;
-        console.log(num + " * " + i + " = " + multiplicacion);
+    num = num.trim();// Elimina espacios al inicio y al final del número.
+    const ul = document.createElement('ul');// Crea un elemento de lista desordenada.
+    for (let i = 0; i <= 10; i++) { // Bucle que itera de 0 a 10.
+        const multiplicacion = num * i;// Calcula la multiplicación del número ingresado por 'i'.
+        console.log(num + " * " + i + " = " + multiplicacion);// Muestra la multiplicación en la consola.
 
-        const li = document.createElement('li');
-        li.textContent = num + " * " + i + " = " + multiplicacion;
-        ul.appendChild(li);
+        const li = document.createElement('li');// Crea un elemento de lista.
+        li.textContent = num + " * " + i + " = " + multiplicacion;// Asigna el texto de la multiplicación al elemento de lista.
+        ul.appendChild(li); // Agrega el elemento de lista a la lista desordenada.
     }
-    const resultadoElement = document.getElementById("resultado");
-    resultadoElement.innerText = '';
-    resultadoElement.appendChild(ul);
+    const resultadoElement = document.getElementById("resultado");// Obtiene el elemento con el ID "resultado".
+    resultadoElement.innerText = '';// Borra el contenido actual del elemento.
+    resultadoElement.appendChild(ul);// Agrega la lista desordenada al elemento con ID "resultado".
 }
